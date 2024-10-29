199e0f7f784f0747c3afff25127c588380e36b4a
weirenran <1392465152@qq.com>
2024-10-29 19:48:31 +0800

    我是weirenran

--- app/src/main/java/org/goldfish/collaborationapplication/weirenran.java
+++ app/src/main/java/org/goldfish/collaborationapplication/weirenran.java
@@ -3,5 +3,6 @@ package org.goldfish.collaborationapplication;
 public class weirenran {
     public static void main(String[] args) {
         System.out.println("我是郑岳平");
+        System.out.println("啦啦啦啦啦");
     }
 }
