6c8bf9660af6fa8954fcaeefecffe9286b997c25
misaka <1241973591@qq.com>
2024-10-29 19:47:08 +0800

    我是你爸爸

--- app/src/main/java/org/goldfish/collaborationapplication/misaka.java
+++ app/src/main/java/org/goldfish/collaborationapplication/misaka.java
@@ -3,5 +3,6 @@ package org.goldfish.collaborationapplication;
 public class misaka {
     public static void main(String[] args) {
         System.out.println("misaka");
+        System.out.println("im yr father");
     }
 }
